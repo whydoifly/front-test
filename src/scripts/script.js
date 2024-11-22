b558c9b1c78d7ea182432a08073d1bf69ad86c0b
whydoifly <netpreecher@gmail.com>
2024-11-22 10:50:39 -0300

    Сверстал страницу по дизайну, добавил стили и скрипт

---
+++ src/scripts/script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", (event) => {
+  console.log("whydoifly");
+});
+
+document.querySelector('.login-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+});
